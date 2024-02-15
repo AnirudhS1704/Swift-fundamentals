3220535ed2e6da296132a0ff7a6877df150723e0
Anirudh S <anirudh.suma2003@gmail.com>
2024-02-15 22:47:37 +0530

    Update demo.swift

--- demo.swift
+++ demo.swift
@@ -5,3 +5,21 @@ let firstName = name[name.startIndex..<spaceIndex!]
 let lastName = name[name.index(after: spaceIndex!)...]
 print(firstName)
 print(lastName)
+
+# print even and odd using switch
+let n = 5
+let c = n % 2
+
+switch c{
+    case 0: print("even")
+    default: print("odd")          
+}
+
+# sum of digits
+var num = 12345
+var num1 = String(num)
+var sum=0
+for i in num1{
+    sum += Int(String(i))!
+}
+print(sum)
