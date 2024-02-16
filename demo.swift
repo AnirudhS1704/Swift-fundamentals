b3f91af75bb4c4c3d0f853c9dcf5ef4886579edf
Anirudh S <anirudh.suma2003@gmail.com>
2024-02-16 18:18:42 +0530

    Update demo.swift

--- demo.swift
+++ demo.swift
@@ -23,3 +23,52 @@ for i in num1{
     sum += Int(String(i))!
 }
 print(sum)
+
+
+# calculate full name
+func calculateFullName(_ firstName: String, _ lastName: String) -> (String, Int){
+    let fullName = firstName + " " + lastName
+    return (fullName, fullName.count)
+}
+var fullName = calculateFullName("Tony", "Stark")
+print(fullName)
+
+# first recurring character
+var s = "EXAMPLE"
+var charset = Set<Character>()
+for i in s{
+    if s.contains(i){
+        print(i)
+        break
+    }
+    else{
+        charset.insert(i)
+    }
+}
+
+# sort by last name
+struct Employee { 
+ var firstName: String 
+ var lastName: String 
+ var designation: String 
+ var phoneNumber: Int 
+} 
+
+var employeeList:Array<Employee> = [
+    Employee(firstName: "A", lastName: "B", designation:"X", phoneNumber: 123456),
+    Employee(firstName: "D", lastName: "S", designation:"X", phoneNumber: 123456),
+    Employee(firstName: "E", lastName: "C", designation:"X", phoneNumber: 123456),
+    Employee(firstName: "O", lastName: "A", designation:"X", phoneNumber: 123456)
+]
+
+employeeList.sort(){
+    $0.lastName < $1.lastName
+}
+
+for i in employeeList{
+    print(i.firstName)
+    print(i.lastName)
+    print(i.designation)
+    print(i.phoneNumber)
+    print()
+}
